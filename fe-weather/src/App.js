5a20cc3c7f405b6a3983cedb7cf7e86389549c95
Your Name <susi.franco@cerebral.com>
2024-05-20 23:05:21 -0500

    add login conditional to app

--- fe-weather/src/App.js
+++ fe-weather/src/App.js
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
 import CityInput from './components/City';
 import TemperatureDisplay from './components/TemperatureDisplay';
+import Login from './components/Login';
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   const [temperature, setTemperature] = useState('');
   const [error, setError] = useState('');
 
-  const handleTemperatureUpdate = (temp) => {
-    setTemperature(temp);
-  };
-
-  const handleError = (errorMsg) => {
-    setError(errorMsg);
+  const handleLogin = () => {
+    setLoggedIn(true);
   };
 
   return (
     <div>
       <h1>Weather Beats</h1>
-      <CityInput onTemperatureUpdate={handleTemperatureUpdate} onError={handleError} />
-      <TemperatureDisplay temperature={temperature} error={error} />
+      {loggedIn ? (
+        <>
+          <CityInput onTemperatureUpdate={setTemperature} onError={setError} />
+          <TemperatureDisplay temperature={temperature} error={error} />
+        </>
+      ) : (
+        <div>
+          <Login onLogin={handleLogin} />
+        </div>
+      )}
     </div>
   );
 };
