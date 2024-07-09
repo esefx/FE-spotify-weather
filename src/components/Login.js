19b64165181ed6352e11d210255a76f528cd6940
Your Name <susi.franco@cerebral.com>
2024-07-09 16:29:19 -0500

    move spotify redirect to frontend

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,34 @@ import axios from 'axios';
 const Login = ({ onLogin }) => {
     const [error, setError] = useState('');
 
+    useEffect(() => {
+        // Function to extract code from URL query parameters
+        const extractCode = () => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const code = urlParams.get('code');
+            if (code) {
+                // Clean up URL
+                window.history.pushState({}, document.title, window.location.pathname);
+                // Make a call to the backend with the code
+                sendCodeToBackend(code);
+            }
+        };
+
+        // Function to send the authorization code to the backend
+        const sendCodeToBackend = async (code) => {
+            try {
+                const response = await axios.post('https://be-spotify-weather.onrender.com/callback', { code });
+                // Handle response from backend, e.g., setting user session
+                onLogin(); // Call the onLogin prop or update state as needed
+            } catch (error) {
+                setError('Failed to complete Spotify login. Please try again.');
+            }
+        };
+
+        extractCode();
+    }, [onLogin]);
+
+
     const handleLogin = async () => {
         try {
             const response = await axios.get('https://be-spotify-weather.onrender.com/login');
@@ -11,21 +39,6 @@ const Login = ({ onLogin }) => {
             document.cookie = `session_id=${session_id}`;
             const authUrl = response.data.auth_url;
             window.location.href = authUrl;
-            // const popup = window.open(authUrl, 'Spotify Login', 'width=800,height=600');
-    
-            // window.addEventListener('message', (event) => {
-            //     if (event.data === 'loginSuccess') {
-            //         clearInterval(popupTick);
-            //         popup.close(); // Ensure the popup is closed
-            //         onLogin(); // Call the onLogin prop or update state as needed
-            //     }
-            // }, false);
-    
-            // const popupTick = setInterval(() => {
-            //     if (popup.closed) {
-            //         clearInterval(popupTick);
-            //     }
-            // }, 500);
         } catch (error) {
             setError('Failed to initiate Spotify login. Please try again.');
         }
