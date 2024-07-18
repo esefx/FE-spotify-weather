702e76933ea82721a96286945e72b190a55d567d
Your Name <susi.franco@cerebral.com>
2024-07-18 17:54:57 -0500

    style playlist

--- src/components/SpotifyPlaylist.js
+++ src/components/SpotifyPlaylist.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 
 const SpotifyPlaylist = ({ playlistId }) => {
   if (!playlistId) return null; // Don't render if no playlist ID is provided
 
   const src = `https://open.spotify.com/embed/playlist/${playlistId}`;
+
   return (
-    <div className="spotify-playlist-container">
-      <iframe
-        src={src}
-        width="400" // Increased width
-        height="600" // Increased height
-        allowtransparency="true"
-        allow="encrypted-media"
-        title="Spotify Playlist created from city's weather data."
-      ></iframe>
-    </div>
+    <Box display="flex" justifyContent="center" mt={4} mb={4}>
+      <div className="spotify-playlist-container">
+        <iframe
+          src={src}
+          width="400"
+          height="600"
+          allowtransparency="true"
+          allow="encrypted-media"
+          title="Spotify Playlist created from city's weather data."
+        ></iframe>
+      </div>
+    </Box>
   );
 };
 
