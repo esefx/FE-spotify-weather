75c5e97763d55b860e477e834c9bd59e45ec4f3c
Your Name <susi.franco@cerebral.com>
2024-07-18 18:15:32 -0500

    add spotify logo

--- src/components/SpotifyPlaylist.js
+++ src/components/SpotifyPlaylist.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const SpotifyPlaylist = ({ playlistId }) => {
   if (!playlistId) return null; // Don't render if no playlist ID is provided
@@ -7,7 +8,18 @@ const SpotifyPlaylist = ({ playlistId }) => {
   const src = `https://open.spotify.com/embed/playlist/${playlistId}`;
 
   return (
-    <Box display="flex" justifyContent="center" mt={4} mb={4}>
+    <Box display="flex" flexDirection="column" alignItems="center" mt={4} mb={4}>
+      {/* Spotify logo with text */}
+      <Typography variant="caption" gutterBottom>
+        Powered by
+        <img
+          src="/assets/spotify-logo.svg" 
+          alt="Spotify Logo"
+          style={{ width: 80, marginLeft: 8, marginRight: 8 }}
+        />
+        Spotify
+      </Typography>
+      {/* Embedded Spotify playlist */}
       <div className="spotify-playlist-container">
         <iframe
           src={src}
