2a1e07a68f13845ac8e2e82e790dbc5f9a3b7d96
Your Name <susi.franco@cerebral.com>
2024-07-18 17:11:09 -0500

    add materialUI index.html css baseline

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline'; // Import CssBaseline from Material UI
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline /> {/* Include CssBaseline as the first component */}
     <App />
   </React.StrictMode>
 );
